2dcb4582dcae281d69d01aca17730d30dc30a9b1
RClement5700 <Rclement5700@gmail.com>
2020-05-21 21:43:25 -0700

    completed Exoplayer logic

--- app/src/main/java/crysalis/example/crysalisbakingapp/StepFragment.java
+++ app/src/main/java/crysalis/example/crysalisbakingapp/StepFragment.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,12 +18,7 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class StepFragment extends Fragment {
-
     @BindView(R.id.img_btn_close_step) ImageButton img_btn_close_step;
     @BindView(R.id.tv_step_instruction) TextView tv_step_instruction;
     @BindView(R.id.previous_btn) Button previous_btn;
@@ -39,72 +36,74 @@ public class StepFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_step, container, false);
         ButterKnife.bind(this, v);
         container.bringToFront();
+
         ArrayList<Step> steps;
         int position;
-        String description = null;
-        String thumbnailUrl;
-        String videoUrl;
-        StepFragment nextFragment;
-        StepFragment previousFragment;
+        String description;
+        final String thumbnailUrl;
+        final String videoUrl;
+        final String nextThumbnailUrl;
+        final String nextVideoUrl;
+        final String previousThumbnailUrl;
+        final String previousVideoUrl;
         final int nextPosition;
         final int previousPosition;
 
-        if (getFragmentManager() != null) {
-            getFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.vid_view_container, new VideoViewFragment())
-                    .commit();
-        }
         if (getArguments() != null) {
             steps = (ArrayList<Step>) getArguments().getSerializable("steps");
             position = getArguments().getInt("position");
-            System.err.println("position: " + position);
             description = steps.get(position).description;
-//            thumbnailUrl = steps.get(position).thumbnailUrl;
-//            videoUrl = steps.get(position).videoUrl;
+            thumbnailUrl = steps.get(position).thumbnailUrl;
+            videoUrl = steps.get(position).videoUrl;
+            if (getFragmentManager() != null) {
+                Bundle bundle = getArguments();
+                bundle.putString("thumbnailUrl", thumbnailUrl);
+                bundle.putString("videoUrl", videoUrl);
+                VideoViewFragment vvFragment = new VideoViewFragment();
+                vvFragment.setArguments(bundle);
+                getFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.vid_view_container, vvFragment)
+                        .commit();
+            }
 
             if (position == 0) {
                 previousPosition = steps.size() - 1;
                 nextPosition = 1;
+                nextVideoUrl = steps.get(nextPosition).videoUrl;
+                nextThumbnailUrl = steps.get(nextPosition).thumbnailUrl;
+                previousVideoUrl = steps.get(previousPosition).videoUrl;
+                previousThumbnailUrl = steps.get(previousPosition).thumbnailUrl;
+
             }
             else if (position == steps.size() - 1) {
                 previousPosition = steps.size() - 2;
                 nextPosition = 0;
+                nextVideoUrl = steps.get(nextPosition).videoUrl;
+                nextThumbnailUrl = steps.get(nextPosition).thumbnailUrl;
+                previousVideoUrl = steps.get(previousPosition).videoUrl;
+                previousThumbnailUrl = steps.get(previousPosition).thumbnailUrl;
             }
 
             else {
                 previousPosition = position - 1;
                 nextPosition = position + 1;
+                nextVideoUrl = steps.get(nextPosition).videoUrl;
+                nextThumbnailUrl = steps.get(nextPosition).thumbnailUrl;
+                previousVideoUrl = steps.get(previousPosition).videoUrl;
+                previousThumbnailUrl = steps.get(previousPosition).thumbnailUrl;
             }
             tv_step_instruction.setText(description);
             next_btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Bundle bundle = getArguments();
-                    bundle.remove("position");
-                    bundle.putInt("position", nextPosition);
-                    System.err.println("nextPosition: " + nextPosition);
-                    StepFragment stepFragment = new StepFragment();
-                    stepFragment.setArguments(bundle);
-                    getFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.step_container, stepFragment)
-                            .commit();
+                    directionButtonPressed(nextVideoUrl, nextThumbnailUrl, nextPosition);
                 }
             });
             previous_btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Bundle bundle = getArguments();
-                    bundle.remove("position");
-                    bundle.putInt("position", previousPosition);
-                    System.err.println("previousPosition: " + previousPosition);
-                    StepFragment stepFragment = new StepFragment();
-                    stepFragment.setArguments(bundle);
-                    getFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.step_container, stepFragment)
-                            .commit();
+                    directionButtonPressed(previousVideoUrl, previousThumbnailUrl, previousPosition);
                 }
             });
         }
@@ -118,6 +117,37 @@ public class StepFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    private void directionButtonPressed(String videoUrl, String thumbnailUrl, int position) {
+
+        Bundle bundle = getArguments();
+        bundle.remove("position");
+        bundle.remove("thumbnailUrl");
+        bundle.remove("videoUrl");
+        bundle.putInt("position", position);
+        if (thumbnailUrl.length() > 0) {
+            bundle.putString("thumbnailUrl", thumbnailUrl);
+        }
+        else {
+            bundle.putString("thumbnailUrl", null);
+        }
+        if (videoUrl.length() > 0) {
+            bundle.putString("videoUrl", videoUrl);
+            System.err.println("videoUrl: " + videoUrl);
+        }
+        else {
+            bundle.putString("videoUrl", null);
+        }
+        VideoViewFragment vvFragment = new VideoViewFragment();
+        StepFragment stepFragment = new StepFragment();
+        vvFragment.setArguments(bundle);
+        stepFragment.setArguments(bundle);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.step_container, stepFragment)
+                .replace(R.id.vid_view_container, vvFragment)
+                .commit();
     }
 
 }
--- app/src/main/java/crysalis/example/crysalisbakingapp/VideoViewFragment.java
+++ app/src/main/java/crysalis/example/crysalisbakingapp/VideoViewFragment.java
@@ -1,6 +1,7 @@
 package crysalis.example.crysalisbakingapp;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class VideoViewFragment extends Fragment {
 
+    @BindView(R.id.iv_placeholder_image) ImageView placeholder_image;
+    @BindView(R.id.video_view) PlayerView videoView;
 
     public VideoViewFragment() {
         // Required empty public constructor
@@ -25,7 +39,40 @@ public class VideoViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_view, container, false);
+        View v =  inflater.inflate(R.layout.fragment_video_view, container, false);
+        ButterKnife.bind(this, v);
+        if (getArguments() != null) {
+            String videoPath = getArguments().getString("videoUrl");
+            if (!videoPath.isEmpty()) {
+                placeholder_image.setVisibility(View.INVISIBLE);
+                videoView.setVisibility(View.VISIBLE);
+                videoView.bringToFront();
+                if (getActivity() != null) {
+                    SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(getActivity());
+                    String userAgent = Util.getUserAgent(getActivity(),
+                            getActivity().getResources().getString(R.string.app_name));
+//                DefaultDataSourceFactory defdataSourceFactory =
+//                        new DefaultDataSourceFactory(getActivity(), userAgent);
+                    DataSource.Factory dataSourceFactory =
+                            new DefaultDataSourceFactory(getActivity(), userAgent);
+                    Uri uriOfContentUrl = Uri.parse(videoPath);
+                    System.err.println("videoPath: " + videoPath);
+                    MediaSource mediaSource = new ProgressiveMediaSource
+                            .Factory(dataSourceFactory)
+                            .createMediaSource(uriOfContentUrl);
+                    player.prepare(mediaSource);
+                    player.setPlayWhenReady(true);
+                    videoView.setPlayer(player);
+                }
+            }
+
+            else {
+                placeholder_image.setVisibility(View.VISIBLE);
+                videoView.setVisibility(View.INVISIBLE);
+                placeholder_image.bringToFront();
+            }
+        }
+        return v;
     }
 
 }
